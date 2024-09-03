ce7a4ce6d0bccb7e53791e58ad327b828709d742
OlgaOvcharenko <ovcharenko.folga@gmail.com>
2024-09-03 22:55:07 +0200

    Rerun some experiments (SIGTERM)

--- night2.sh
+++ night2.sh
@@ -1,2 +1,4 @@
-python main.py --multirun +exp='dimension_ablation_h1-16'
-python main.py --multirun +exp='dimension_ablation_h1-8'
+nohup python main.py --multirun +experiment=multimodal_Neurips_base_ablation +cluster=slurm &
+nohup python main.py --multirun +experiment=multimodal_Neurips_bknn_augmentation_ablation +cluster=slurm &
+nohup python main.py --multirun +experiment=multimodal_Neurips_base_ablation_qr +cluster=slurm &
+nohup python main.py --multirun +experiment=multimodal_Neurips_bknn_augmentation_ablation_qr +cluster=slurm &
