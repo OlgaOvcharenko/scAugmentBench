5947e8a11168778ab1596ed71474de8ebd5582a9
OlgaOvcharenko <ovcharenko.folga@gmail.com>
2024-12-14 18:18:30 +0100

    Add changes

--- utils/merge_results_bc.py
+++ utils/merge_results_bc.py
@@ -62,6 +62,55 @@ def compare_model_architectures(projection, combine, data):
     stds = stds.drop(remove_cols, axis=1).round(3)
     return averages, stds
 
+def compare_model_architectures_single(projection, combine, data):
+    metrics = ['Isolated labels', 'Leiden NMI', 'Leiden ARI', 'Silhouette label', 'cLISI', 'Silhouette batch', 'iLISI', 'KBET', 'Graph connectivity', 'PCR comparison']
+
+    cols = []
+    for c in combine:
+        for p in projection:
+            for d in data:
+                for metric in metrics:
+                    cols.append(f"{d}_{c}_{p}_{metric}")
+                    
+    df_list = [pd.DataFrame(index=models_list, columns=cols), pd.DataFrame(index=models_list, columns=cols), pd.DataFrame(index=models_list, columns=cols), pd.DataFrame(index=models_list, columns=cols), pd.DataFrame(index=models_list, columns=cols)]
+    for model in models_list:
+        for c in combine:
+            for p in projection:
+                for d in data:
+                    PATH = experiment + '/' + d + '/' + model + '/' + pipeline + '/' + f"integrate_{c}_RNA_False_projection_{p}/"
+                    result_paths = [y for x in os.walk(PATH) for y in glob(os.path.join(x[0], '*.csv'))]
+                    
+                    if len(result_paths) != 1:
+                        print(f"ERROR {len(result_paths)}")
+                        exit()
+
+                    for i, seed_path in enumerate(result_paths):
+                        with open(seed_path, newline='') as csvfile:
+                            reader = csv.DictReader(csvfile)
+                            for row in reader:
+                                row.pop('Bio conservation')
+                                row.pop('Batch correction')
+                                row.pop('Total')
+
+                                for k, v in row.items():
+                                    df_list[i].loc[model, f'{d}_{c}_{p}_{k}'] = float(v)
+
+    df_final = []
+    for df in df_list:
+        df_final.append(scale_result(df, projection, combine, data))
+
+    averages = pd.concat([each.stack() for each in df_final],axis=1)\
+             .apply(lambda x:x.mean(),axis=1)\
+             .unstack()
+    stds = pd.concat([each.stack() for each in df_final],axis=1)\
+             .apply(lambda x:x.std(),axis=1)\
+             .unstack()
+    
+    remove_cols = list(filter(lambda x: ("Bio conservation" not in x) and ("Batch correction" not in x) and ("Total" not in x), list(averages.columns)))
+    averages = averages.drop(remove_cols, axis=1).round(3)
+    stds = stds.drop(remove_cols, axis=1).round(3)
+    return averages, stds
+
 def scale_result(df, projection, combine, data):
     scaled= pd.DataFrame(MinMaxScaler().fit_transform(df), columns=df.columns, index=df.index)
     for c in combine:
@@ -77,7 +126,7 @@ def scale_result(df, projection, combine, data):
                 df[f"{d}_{c}_{p}_Total"] = scaled[f"{d}_{c}_{p}_Total"].copy()
     return df
 
-plot = 2
+plot = 3
 if plot == 1:   
     # Compare CLIP, Add, Concat for backbone  
     averages, stds = compare_model_architectures(projection = ['False'], combine=combine, data=['Neurips-Multiome'])
@@ -94,6 +143,18 @@ elif plot == 2:
     # Compare Concat for backbone and projection
     averages, stds = compare_model_architectures(projection = ['False', 'True'], combine=['add'], data=data)
     dir_path = "results/bc/avg_compare_backbone_projection/"
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+        print("Directory created successfully!")
+    else:
+        print("Directory already exists!")
+    averages.to_csv(f"{dir_path}avg.csv")
+    stds.to_csv(f"{dir_path}std.csv")
+
+elif plot == 3:
+    # Compare Concat for backbone and projection
+    averages, stds = compare_model_architectures_single(projection = ['False'], combine=['concat'], data=data)
+    dir_path = "results/bc/avg_compare_backbone_projection_new/"
     if not os.path.exists(dir_path):
         os.makedirs(dir_path)
         print("Directory created successfully!")
